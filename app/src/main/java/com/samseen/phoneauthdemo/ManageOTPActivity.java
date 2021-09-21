624b8a3dc760ab5afde3cd2f22df4c4e08763577
Samson Akanbi <samson.akanbi@outlook.com>
2021-09-21 20:05:28 +0100

    Project layout setup.

---
+++ app/src/main/java/com/samseen/phoneauthdemo/ManageOTPActivity.java
@@ -0,0 +1,14 @@
+package com.samseen.phoneauthdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageOTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manage_otp_activity);
+    }
+}
