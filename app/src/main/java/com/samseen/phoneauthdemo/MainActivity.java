399e202471a9177128e87aec5fc9e5efd317cf28
Samson Akanbi <samson.akanbi@outlook.com>
2021-09-23 07:07:34 +0100

    Designed the OTP to be sent from Firebase.

--- app/src/main/java/com/samseen/phoneauthdemo/MainActivity.java
+++ app/src/main/java/com/samseen/phoneauthdemo/MainActivity.java
@@ -13,43 +13,11 @@ import com.google.firebase.auth.FirebaseUser;
 import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
-    CountryCodePicker ccp;
-    EditText t1;
-    Button b1;
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        t1 = findViewById(R.id.t1);
-        ccp = findViewById(R.id.ccp);
-        ccp.registerCarrierNumberEditText(t1);
-        b1 = findViewById(R.id.b1);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ManageOTPActivity.class);
-                intent.putExtra("mobile", ccp.getFullNumberWithPlus().replace(" ", ""));
-                startActivity(intent);
-
-            }
-        });
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser == null) {
-            Intent intent = new Intent(MainActivity.this, ManageOTPActivity.class);
-            intent.putExtra("mobile", ccp.getFullNumberWithPlus().replace(" ", ""));
-            startActivity(intent);
-        }
     }
 }
--- app/src/main/java/com/samseen/phoneauthdemo/ManageOTPActivity.java
+++ app/src/main/java/com/samseen/phoneauthdemo/ManageOTPActivity.java
@@ -1,58 +1,82 @@
 package com.samseen.phoneauthdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class ManageOTPActivity extends AppCompatActivity {
 
-    public static final String TAG = "ManageOTPActivity";
-
-    EditText t2;
-    Button b2;
-    String phone_number;
-    private FirebaseAuth auth;
-
+    private Button mVerifyCodeBtn;
+    private EditText otpEdit;
+    private String OTP;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manage_otp_activity);
 
-        t2 = findViewById(R.id.t2);
-        b2 = findViewById(R.id.verify_code_button);
-        phone_number = getIntent().getStringExtra("mobile").toString();
-        auth = FirebaseAuth.getInstance();
+        mVerifyCodeBtn = findViewById(R.id.verify_code_button);
+        otpEdit = findViewById(R.id.verify_code_edit);
 
-        b2.setOnClickListener(new View.OnClickListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        OTP = getIntent().getStringExtra("auth");
+        mVerifyCodeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "Phone number is: "+phone_number);
+                String verification_code = otpEdit.getText().toString();
+                if(!verification_code.isEmpty()) {
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(OTP, verification_code);
+                    signIn(credential);
+                } else {
+                    Toast.makeText(ManageOTPActivity.this, "Please Enter OTP", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
     }
 
+    private void signIn(PhoneAuthCredential credential) {
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    sendToMain();
+                } else {
+                    Toast.makeText(ManageOTPActivity.this, "Verification failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseUser user = auth.getCurrentUser();
-        if(user != null) {
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if(currentUser != null) {
             sendToMain();
         }
     }
 
     private void sendToMain() {
-        Intent mainIntent = new Intent(ManageOTPActivity.this, DashboardActivity.class);
-        startActivity(mainIntent);
+        startActivity(new Intent(ManageOTPActivity.this, MainActivity.class));
         finish();
     }
 }
